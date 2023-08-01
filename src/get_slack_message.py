71cc478ff62d11eae2eac661c6cab0173fd61cc1
nakashima-takeo <tonarinototorototoro8972@gmail.com>
2023-08-01 18:02:32 +0900

    fix slack

--- src/get_slack_message.py
+++ src/get_slack_message.py
@@ -1,27 +1,33 @@
-# from aws.S3 import S3
+import datetime
+
+from aws.S3 import S3
 from slack.slack_usecase import SlackUsecase
 
 
-def main() -> None:
-    slack_usecase = SlackUsecase()
+def save_slack_messages_to_s3() -> None:
+    slack = SlackUsecase()
     source_channel_name: str = "annotation新卒"
-    # report_channel_name: str = "annotation新卒"
+    report_channel_name: str = "annotation新卒"
     search_words: list[str] = ["こんにちは", "こんばんは", "API"]
     search_hours: int = 24
 
     # slackからメッセージを取得する
-    messages = slack_usecase.search_messages(source_channel_name, search_words, search_hours)
+    messages = slack.search_messages(source_channel_name, search_words, search_hours)
     if len(messages) == 0:
         print("メッセージはありませんでした")
         return
 
-    # # メッセージをテキストファイルに書き出す
-    # with open("messages.txt", mode="w") as f:
-    #     f.write("\n".join(messages))
+    # メッセージを一つのstringに整える
+    messages_text: str = ""
+    for message in messages:
+        messages_text += datetime.datetime.fromtimestamp(int(message["ts"]) * 1000).strftime("%Y/%m/%d %H:%M:%S") + "\n"
+        messages_text += slack.get_user(message["user"])["real_name"] + "\n"
+        messages_text += message["text"] + "\n"
+        messages_text += "\n"
 
-    # # S3にテキストファイルをアップロードする
-    # s3 = S3()
-    # s3.upload_file("messages.txt", "messages.txt")
+    # S3にテキストファイルをアップロードする
+    s3 = S3()
+    s3.write_txt("slack_task_bucket_temp", "slack", messages_text)
 
     # # Slackにメッセージを投稿する
-    # slack_usecase.post_message(report_channel_name, "メッセージを保存しました")
+    slack.post_message(report_channel_name, "メッセージを保存しました")
--- src/slack/slack_usecase.py
+++ src/slack/slack_usecase.py
@@ -6,6 +6,7 @@ from slack_infrastructure import SlackInfrastructure
 
 class SlackUsecase:
     def __init__(self):
+        self.users = []
         self.slack_infrastructure = SlackInfrastructure()
 
     def search_messages(self, channel_name: str, search_words: list[str], search_hours: int) -> list[Dict]:
@@ -17,3 +18,19 @@ class SlackUsecase:
         messages = self.slack_infrastructure.get_channel_history(channel["id"], from_unixtime)
         messages = [message for message in messages if any(word in message["text"] for word in search_words)]
         return messages
+
+    def get_user(self, user_id: str) -> Dict:
+        # 既に取得済みのユーザー情報を取得
+        user = next((user for user in self.users if user["id"] == user_id), None)
+        if user is not None:
+            return user
+        # 取得済みでない場合はAPIを叩いて取得
+        new_user = self.slack_infrastructure.get_user_info(user_id)
+        self.users.append(new_user)
+        return new_user
+
+    def post_message(self, channel_name: str, message: str) -> None:
+        channel = self.slack_infrastructure.get_a_channel(channel_name)
+        if channel is None:
+            raise ValueError(f"チャンネル「{channel_name}」が見つかりませんでした")
+        self.slack_infrastructure.post_message(channel, message)
