e26668a24a3fee23ae0594447fdd903a81068bd6
timer <38322674+nakashima-takeo@users.noreply.github.com>
2023-11-07 16:31:16 +0900

    docstringを追加 (#3)

--- mymodules/slack/slack_infrastructure.py
+++ mymodules/slack/slack_infrastructure.py
@@ -6,35 +6,60 @@ from mymodules.aws import SecretsManager
 
 
 class SlackInfrastructure:
+    """
+    Slack APIを利用するためのインフラストラクチャを提供するクラスです。
+    """
+
     def __init__(self):
-        secrets_manager = SecretsManager()
-        slack_api_token = secrets_manager.get_secret("slack", "slack_app_token")
-        # slack_api_token = input("Slack API Token: ")
-        if slack_api_token is None:
+        __secrets_manager = SecretsManager()
+        __slack_api_token = __secrets_manager.get_secret("slack", "slack_app_token")
+        # __slack_api_token = input("Slack API Token: ")
+        if __slack_api_token is None:
             raise ValueError("SLACK_API_TOKENが設定されていません")
-        self.token = slack_api_token
-        self.headersAuth = {
-            "Authorization": "Bearer " + str(self.token),
+        self.__token = __slack_api_token
+        self.__headersAuth = {
+            "Authorization": "Bearer " + str(self.__token),
         }
 
-        self.history_url = "https://slack.com/api/conversations.history"
-        self.channel_list_url = "https://slack.com/api/conversations.list"
-        self.post_message_url = "https://slack.com/api/chat.postMessage"
-        self.thread_url = "https://slack.com/api/conversations.replies"
-        self.user_info_url = "https://slack.com/api/users.info"
+        self.__history_url = "https://slack.com/api/conversations.history"
+        self.__channel_list_url = "https://slack.com/api/conversations.list"
+        self.__post_message_url = "https://slack.com/api/chat.postMessage"
+        self.__thread_url = "https://slack.com/api/conversations.replies"
+        self.__user_info_url = "https://slack.com/api/users.info"
 
     def get_a_channel(self, channel_name: str) -> Dict | None:
+        """
+        指定されたチャンネル名に一致するチャンネルを取得します。
+
+        Parameters
+        ----------
+        channel_name : str
+          取得したいチャンネル名
+
+        Returns
+        -------
+        Dict or None
+          指定されたチャンネル名に一致するチャンネルの情報。見つからなかった場合はNone。
+        """
         channels = self.get_all_channels()
         return next(
             (channel for channel in channels if channel["name"] == channel_name),
         )
 
     def get_all_channels(self) -> List[Dict]:
+        """
+        全てのチャンネルの情報を取得します。
+
+        Returns
+        -------
+        List[Dict]
+          全てのチャンネルの情報を含むリスト。
+        """
         payload: Dict = {
             "types": "public_channel, private_channel",
             "limit": 1000,
         }
-        response = requests.get(self.channel_list_url, headers=self.headersAuth, params=payload)
+        response = requests.get(self.__channel_list_url, headers=self.__headersAuth, params=payload)
         response.raise_for_status()
         if not response.json()["ok"]:
             raise ValueError("チャンネルの取得に失敗しました")
@@ -42,13 +67,28 @@ class SlackInfrastructure:
         return channels
 
     def get_channel_history(self, channel_id: str, from_unixtime: int | None = None) -> List[Dict]:
+        """
+        指定されたチャンネルの履歴を取得します。
+
+        Parameters
+        ----------
+        channel_id : str
+          取得したいチャンネルのID
+        from_unixtime : int or None
+          取得するメッセージの最古のUNIX時間。指定しない場合はNone。
+
+        Returns
+        -------
+        List[Dict]
+          指定されたチャンネルの履歴を含むリスト。
+        """
         payload: Dict = {
             "channel": channel_id,
             "limit": 1000,
         }
         if from_unixtime is not None:
             payload["oldest"] = from_unixtime
-        response = requests.get(self.history_url, headers=self.headersAuth, params=payload)
+        response = requests.get(self.__history_url, headers=self.__headersAuth, params=payload)
         response.raise_for_status()
         if not response.json()["ok"]:
             raise ValueError("チャンネルの取得に失敗しました")
@@ -56,23 +96,55 @@ class SlackInfrastructure:
         return messages
 
     def get_thread_history(self, channel: Dict, original_message: Dict) -> List[Dict]:
+        """
+        指定されたスレッドの履歴を取得します。
+
+        Parameters
+        ----------
+        channel : Dict
+          スレッドが存在するチャンネルの情報
+        original_message : Dict
+          スレッドの親メッセージ
+
+        Returns
+        -------
+        List[Dict]
+          指定されたスレッドの履歴を含むリスト。
+        """
         payload: Dict = {
             "channel": channel["id"],
             "ts": original_message["ts"],
         }
-        response = requests.get(self.thread_url, headers=self.headersAuth, params=payload)
+        response = requests.get(self.__thread_url, headers=self.__headersAuth, params=payload)
         response.raise_for_status()
         messages = response.json()["messages"]
         return messages
 
     def post_message(self, channel: Dict, text: str, original_message: Dict | None = None) -> Dict:
+        """
+        指定されたチャンネルにメッセージを投稿します。
+
+        Parameters
+        ----------
+        channel : Dict
+          メッセージを投稿するチャンネルの情報
+        text : str
+          投稿するメッセージの本文
+        original_message : Dict or None
+          返信するメッセージの情報。指定しない場合はNone。
+
+        Returns
+        -------
+        Dict
+          投稿されたメッセージの情報。
+        """
         payload: Dict = {
             "channel": channel["id"],
             "text": text,
         }
         if original_message is not None:
             payload["thread_ts"] = original_message["thread_ts"]
-        response = requests.post(self.post_message_url, headers=self.headersAuth, params=payload)
+        response = requests.post(self.__post_message_url, headers=self.__headersAuth, params=payload)
         response.raise_for_status()
         message = response.json()
         if not message["ok"]:
@@ -80,10 +152,23 @@ class SlackInfrastructure:
         return message["message"]
 
     def get_user_info(self, user_id: str) -> Dict:
+        """
+        指定されたユーザーの情報を取得します。
+
+        Parameters
+        ----------
+        user_id : str
+          取得したいユーザーのID
+
+        Returns
+        -------
+        Dict
+          指定されたユーザーの情報。
+        """
         payload: Dict = {
             "user": user_id,
         }
-        response = requests.get(self.user_info_url, headers=self.headersAuth, params=payload)
+        response = requests.get(self.__user_info_url, headers=self.__headersAuth, params=payload)
         response.raise_for_status()
         user_info = response.json()["user"]
         return user_info
--- mymodules/slack/slack_usecase.py
+++ mymodules/slack/slack_usecase.py
@@ -5,39 +5,98 @@ from .slack_infrastructure import SlackInfrastructure
 
 
 class SlackUsecase:
+    """
+    Slackの機能を利用するためのユースケースクラスです。
+    """
+
     def __init__(self):
-        self.users = []
-        self.slack_infrastructure = SlackInfrastructure()
+        self.__users = []
+        self.__slack_infrastructure = SlackInfrastructure()
 
     def search_messages(self, channel_name: str, search_words: list[str], search_hours: int) -> list[Dict]:
-        channel = self.slack_infrastructure.get_a_channel(channel_name)
+        """
+        指定したチャンネル内のメッセージを検索します。
+
+        Parameters
+        ----------
+        channel_name : str
+          チャンネル名
+        search_words : list[str]
+          検索する単語のリスト
+        search_hours : int
+          検索する時間範囲（何時間前まで検索するか）
+
+        Returns
+        -------
+        list[Dict]
+          検索結果のメッセージのリスト
+        """
+        channel = self.__slack_infrastructure.get_a_channel(channel_name)
         if channel is None:
             raise ValueError(f"チャンネル「{channel_name}」が見つかりませんでした")
         # search_hours時間前のunixtimeを計算する
         from_unixtime = int((datetime.now() - timedelta(hours=search_hours)).timestamp())
-        messages = self.slack_infrastructure.get_channel_history(channel["id"], from_unixtime)
+        messages = self.__slack_infrastructure.get_channel_history(channel["id"], from_unixtime)
         messages = [message for message in messages if any(word in message["text"] for word in search_words)]
         return messages
 
     def get_thread_history(self, channel_name: str, original_message: Dict) -> list[Dict]:
-        channel = self.slack_infrastructure.get_a_channel(channel_name)
+        """
+        指定したチャンネル内のスレッドの履歴を取得します。
+
+        Parameters
+        ----------
+        channel_name : str
+          チャンネル名
+        original_message : Dict
+          スレッドの親メッセージ
+
+        Returns
+        -------
+        list[Dict]
+          スレッドの履歴のリスト
+        """
+        channel = self.__slack_infrastructure.get_a_channel(channel_name)
         if channel is None:
             raise ValueError(f"チャンネル「{channel_name}」が見つかりませんでした")
-        messages = self.slack_infrastructure.get_thread_history(channel, original_message)
+        messages = self.__slack_infrastructure.get_thread_history(channel, original_message)
         return messages
 
     def get_user(self, user_id: str) -> Dict:
+        """
+        指定したユーザーの情報を取得します。
+
+        Parameters
+        ----------
+        user_id : str
+          ユーザーID
+
+        Returns
+        -------
+        Dict
+          ユーザー情報
+        """
         # 既に取得済みのユーザー情報を取得
-        user = next((user for user in self.users if user["id"] == user_id), None)
+        user = next((user for user in self.__users if user["id"] == user_id), None)
         if user is not None:
             return user
         # 取得済みでない場合はAPIを叩いて取得
-        new_user = self.slack_infrastructure.get_user_info(user_id)
-        self.users.append(new_user)
+        new_user = self.__slack_infrastructure.get_user_info(user_id)
+        self.__users.append(new_user)
         return new_user
 
     def post_message(self, channel_name: str, message: str) -> None:
-        channel = self.slack_infrastructure.get_a_channel(channel_name)
+        """
+        指定したチャンネルにメッセージを投稿します。
+
+        Parameters
+        ----------
+        channel_name : str
+          チャンネル名
+        message : str
+          投稿するメッセージ
+        """
+        channel = self.__slack_infrastructure.get_a_channel(channel_name)
         if channel is None:
             raise ValueError(f"チャンネル「{channel_name}」が見つかりませんでした")
-        self.slack_infrastructure.post_message(channel, message)
+        self.__slack_infrastructure.post_message(channel, message)
